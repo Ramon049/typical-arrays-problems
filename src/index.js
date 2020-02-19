ea6c9424f3120f1bfade0429726faaeece1e1c89
Roman Koshla <koshla.roman10@gmail.com>
2020-02-20 00:16:11 +0400

    feat: add solve for tyoical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(arguments.length == 0 || array.length == 0){
+    return 0
+  }
+  return Math.min(...array)
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(arguments.length == 0 || array.length == 0){
+    return 0
+  }
+  return Math.max(...array)
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(arguments.length == 0 || array.length == 0){
+    return 0
+  }
+  let sum = array.reduce((prev, cur) => cur += prev);
+  let averrage = sum / array.length;
+
+  return averrage;
 }
